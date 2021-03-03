80174ddea7b55557b258f826b79f25d51e3b7234
tolutronics <toluxy100@gmail.com>
2021-03-03 15:33:11 +0100

    interview coding test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { IonSlides } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  sliderConfig={
+    slidesPerView:2.5,
+    spaceBetween:5,
+    // loop:true,
+    pagination:false
+  }
+
   constructor() {}
+
 
 }
